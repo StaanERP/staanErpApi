629a9d4efcc5c8599f6bd82dff0363c4af2dfebb
StaanERP <jegathish.e@staan.in>
2024-03-05 15:59:49 +0530

    update POS datas1

--- Staanenquiryfromwithazure/settings.py
+++ Staanenquiryfromwithazure/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8yl-hpyid+th##7(-8h-dhom9m9^q*8+uesbvybfc50b-t9v4h
 DEBUG = True
 
 # ALLOWED_HOSTS = ["52.66.208.179",'www.maxnik.org',"maxnik.org"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["52.66.208.179",'www.maxnik.org',"maxnik.org"]
 # SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -60,12 +60,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-
-    "http://localhost:3000",
+"https://www.staanerp.com"
+    # "http://localhost:3000",
 
 
 ]
-#    # 'https://www.staanerp.com',
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOW_METHODS = [
@@ -84,9 +83,7 @@ CORS_ALLOW_HEADERS = [
     'Origin',
     'User-Agent',
 ]
-# GRAPHENE = {
-#     "SCHEMA":  "schema.schema"
-# }
+
 CORS_ALLOW_CREDENTIALS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -121,22 +118,22 @@ WSGI_APPLICATION = 'Staanenquiryfromwithazure.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Staan2',
-#         'USER': 'StaanErp',
-#         'PASSWORD': 'StaanDev',
-#         'HOST': 'staandatabase-2.cjkxnr9xutf0.ap-south-1.rds.amazonaws.com',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Staan2',
+        'USER': 'StaanErp',
+        'PASSWORD': 'StaanDev',
+        'HOST': 'staandatabase-2.cjkxnr9xutf0.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
